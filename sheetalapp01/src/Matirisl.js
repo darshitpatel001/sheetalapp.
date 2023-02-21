32a0c716553fc889fe333f9a7cda6961cf93160c
darshit akbari <darshitakbari9999@gmail.com>
2023-02-21 17:38:46 +0530

    c

--- sheetalapp01/src/Matirisl.js
+++ sheetalapp01/src/Matirisl.js
@@ -32,25 +32,25 @@ export default function Matirisl() {
                 </RadioGroup>
             </FormControl>
             <Switch {...label} defaultChecked />
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />   
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
             <Stack sx={{ width: '100%' }} spacing={2}>
-      <Alert severity="error">
-        <AlertTitle>Error</AlertTitle>
-        This is an error alert — <strong>check it out!</strong>
-      </Alert>
-      <Alert severity="warning">
-        <AlertTitle>Warning</AlertTitle>
-        This is a warning alert — <strong>check it out!</strong>
-      </Alert>
-      <Alert severity="info">
-        <AlertTitle>Info</AlertTitle>
-        This is an info alert — <strong>check it out!</strong>
-      </Alert>
-      <Alert severity="success">
-        <AlertTitle>Success</AlertTitle>
-        This is a success alert — <strong>check it out!</strong>
-      </Alert>
-    </Stack>
+                <Alert severity="error">
+                    <AlertTitle>Error</AlertTitle>
+                    This is an error alert — <strong>check it out!</strong>
+                </Alert>
+                <Alert severity="warning">
+                    <AlertTitle>Warning</AlertTitle>
+                    This is a warning alert — <strong>check it out!</strong>
+                </Alert>
+                <Alert severity="info">
+                    <AlertTitle>Info</AlertTitle>
+                    This is an info alert — <strong>check it out!</strong>
+                </Alert>
+                <Alert severity="success">
+                    <AlertTitle>Success</AlertTitle>
+                    This is a success alert — <strong>check it out!</strong>
+                </Alert>
+            </Stack>
         </div>
     );
 }
