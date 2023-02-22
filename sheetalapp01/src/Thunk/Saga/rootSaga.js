d2d7e25b6b88f86dd816d63b3e711ea2ef075ea0
darshit akbari <darshitakbari9999@gmail.com>
2023-02-22 12:12:48 +0530

    xxx

--- sheetalapp01/src/Thunk/Saga/rootSaga.js
+++ sheetalapp01/src/Thunk/Saga/rootSaga.js
@@ -1,6 +1,5 @@
 import { put, takeEvery, takeLatest, delay, all } from 'redux-saga/effects';
-import {incrementWatcher, multiplicationWatcher,} from './counterSaga' 
-import {devisionWatcher}  from './counterSaga';
+import {incrementWatcher, multiplicationWatcher,devisionWatcher} from './counterSaga' 
 import { workFetchPosts } from './PostSaga';
 export default function* rootSaga()
 {
